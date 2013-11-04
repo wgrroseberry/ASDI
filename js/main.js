8e4a2a25cfb64925932dd0b88abeea0089ade519
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-11-03 19:54:01 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,166 +1,201 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>my new Mecai site testing commit</title>
-    
-    <!-- StyleSheets under here -->
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link href="http://code.jquery.com/mobile/1.0/jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css">
-    
-    <!-- JavaScript is normally at the end of the body, except for Modernizr which enables HTML5 elements & feature detects -->
+ 
+   $('#index.html').on('pageinit', function(){
 	
-<script src="http://code.jquery.com/jquery-1.6.4.min.js" type="text/javascript"></script>
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css" />
-    
-    <script src="js/libs/modernizr.custom.min.js"></script>
-</head>
+	      console.log("Line 26 on pageinit funtion");
 
-<body>
-
-        
-<section data-role="page" id="home">
-    	
-        <div data-role="header">
-           <h1> Mecai </h1>
-          
-           <p> Welcome to Create a Creature</p>
-      </div>
-       
-        
-            
-            	<div data-role="content">
-                
-        	  		<form action="#" method="post" id="recordmakeCreature">
-                    
-                  
-    <label for="firstName">FirstName:</label>
-    <input id="firstName" name="firstName" type="text" placeholder="FirstName" required autofocus/>
-    
-    <label for="lastName">LastName:</label>
-    <input id="lastName" name="lastName" type="text" placeholder="LastName" required />
-    
-    <label for="email">Email:</label>
-    <input id="email" name="email" type="email" placeholder="hello@gmail.com" required />
-    
-    <label for="name"></label>
-    Name of Creature:
-    <input id="name" name="name" type="text" required/>
-    
-    <label for="date"></label>
-   Birthday of Creature: <input id="date" name="date" type="date" required/>
-   
-   <div data-role="fieldcontain">
-  <label for="textarea">Create A Creature Notes:</label>
-  <textarea cols="40" rows="8" name="textarea" id="textarea"></textarea>
-
-<!-- original submit button
-<button id="submit">Submit</button>
--->
-</div>
-    
+		var rcform = $('#recordmakeCreature');
+		    myCreatureErrorLink = $('#mycreatureErrorslink')
+		    rcform.validate({
+			invalidHandler: function(form, validator) {
+				myCreatureErrorLink.click();
+			},
+			submitHandler: function() {
+		var data = rcform.serializeArray();
+			storeData(key);
+			console.log("pageinit function Line 15 store data right after this storeData line is run");
+		}
+	});
+	console.log("inside the pageinit function");
+	//any other code needed for addItem page goes here
+	
+});
   
-    
-    <input type="submit" value="saveMyCreature" data-theme="b" />
-    <input type="reset" value="reset" data-theme="a" data-inline="true" />
-    
-    
-<a href="#creatureErrors" id="mycreatureErrorslink"  data-rel="dialog">Open Errors</a> 
-</form>
-              		
-                    	
-                      </div>
-                      
-                      <div data-role="dialog" id="creatureErrors">
-        
-        
-         <div data-role="header" data-position="fixed">
-             <h1>Required Fields</h1>
-         </div>
-        <div data-role="content">
-        	 <ul>
-        	 	<li>Test</li>
-        	 	
-        	 </ul>
-            </div>
-         </div>
-         
-                    
-                    
-           
-       
-                 
-                   
+  
+  
+  
+  $(document).ready(function(){
+	
+	var rcform = $('#recordmakeCreature');
+	
+	rcform.validate( {
+		invalidHandler: function(form, validator) {},
+		submitHandler: function() {
+			var data = rcform.serializeArray();
+			
+			console.log(" Document.ready function parse data right after this storeData line is run line 49");
+			parseCreatureForm(data);
+			console.log("store data this inside the document.ready function line 51");
+			storeData(data);
+			}
+		});
+	
 
-                  
-                    
-       
+ var parseCreatureForm = function(key){
+	// uses form data here;
+	console.log(key);
+	};
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ function displayData() {
+	 $('recordmakeCreature');
+	 
+	 $('#recordmakeCreature').html();
+	 $('#myNewId').html('<p>Set this paragraph</p>');
+	 
+
+	 console.log("inside the displayData funtion on line 61");
+ 
+	 
+	 var name = window.localStorage['name'];
+     var birthday = window.localStorage['birthday'];
+     var gender = window.localStorage['gender'];
+	 //Converting
+	 //document.getElementById('name-div').innerText = name ;// convert to jquery
+	 
+	 // html form example <div id="name-div"></div>
+	 
+	 // You can do that by
+	 // $('#myBtn').click(function(e){ // code goes here }); 
+	 
+	 $("#button_id").click(function() { 
+alert(localStorage.getItem('local_key')); // #local_key i have at the time of storing data into local storage using localStorage.setItem
+
+})
+	 
+	
+	 
+	 
+	 
+	 
+	 
+	 
+	 }
+	 console.log("The form tag is here on line 70");
+	 console.log(recordmakeCreature);
+	 
+	 displayData();
+ 
+ 
+ 
+
+ 
+ 	
+ 
+
+ 
+ 
+ 
+ 
+ 
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 
 
 
 
-             </section>
-             
 		
-    <div id="main">
-    	<h1>Display:</h1>
-        </div>
-    <div data-role="fieldcontain">
-    	<input type="submit" value="SaveCreature" data-theme="b" />
-        
-        <input type="reset" value="Reset" data-theme="a" data-inline="true" />
-        
-        <input type="button" value="clearStorage" data-theme="b" data-inline="true" />
-        		
-
-		      </div>
-             
-              
-                    <footer>
-                   
-                    	<nav>
-                        	<ul>
-                            	<li><a href="#" id="CreatureClear">Clear my Creatures</a></li>
-                                <li><a href="#" id="Add-A-Creature">Add a Creature</a></li>
-                                <li><a href="#" id="view">Display-A-Creature</a></li>
-                                </ul>
-                                </nav>
-                                </footer>
-                               
-               
-                       	
-                       	
-                    
-              <!--     <a href="#creatureErrors" id="mycreatureErrorslink"  data-rel="dialog">Open Errors</a>       
-             -->
-          
-                             
-              <!-- JavaScript under here -->
-           
-           <!--   Not needed link does not work at the moment
-            <link rel="stylesheet" href="/js/themes/greyblue.min.css" />
-           -->
-        <!-- <script type="text/jquery" src="js/libs/jquery-2.0.3.min.js"></script>
-         
-   
-        
-     -->
-     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
-          <script type="text/javascript" src="js/libs/jquery.validate.min.js"></script>
-        <script type="text/javascript" src="js/main.js"></script>
-   
-        
-        
-       
- 
- 
- 
- 
- </body>
-
-</html>                             
 
 
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(key){
+	    
+		if (!key) {
+		// if there is no key, this mean this is a brand new item and requires a new key
+		
+		var id = Math.floor(Math.random()*1000000001);
+		
+		}else{
+		// Set the id to the existing key we're editing so that it will save over the data
+		// the key is the same key that's been passed along from the editSubmit event handler
+		//to the validate function, and then passed here, into the storedata function
+		id = key;
+		console.log("this is the key value on line 93 where is key is established key" + key);
+		}  
+		// Geting all the form field values
+		// Object will contain a array and input values.
+		
+//		getCheckboxes();
+		var item           = {};
+	 	
+        item.FirstName             =["FirstName", $('#FirstName').val()];
+		item.LastName              =["LastName", $('#LastName').val()];
+        item.email                 =["Email:", $('#Email').val()];
+	    item.NameofCreature        =["Creature:", $('#CreatureName').val()];		
+		item.Dates                 =["Date:", $('#Dates').val()];
+		item.notes                 =["Notes:", $('#notes').val()];
+	
+	/*	item.gender                =["Gender:", genderValue];   future reference*/
+		item.iq                    =["IQ", $('#iq').val()];
+		console.log("right after the getCheckboxes Store Data funtion");
+		console.log("the key = " + key);
+	
+		// Save information into local storage
+		// Use stringify to convert our object to a string.
+		
+		
+		
+		// save to local storage
+		localStorage.setItem(id, JSON.stringify(item));
+		
+		
+		alert("Your faviate Creature is saved!");
+	
+	
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.")
+		}else{
+			localStorage.clear();
+			alert("All the Creature favorates have been cleared");
+			window.location.reload();
+			return false;	
+		}
+	
+
+};
+
+
+});
