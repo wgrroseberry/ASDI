776ddb0ba68d92f57ca03ab6f6e2bbdc2b0a5110
B70718 <wgrroseberry@fullsail.edu>
2013-11-04 00:36:48 -0700

    Working on local stoage on display  to page

--- js/main.js
+++ js/main.js
@@ -19,7 +19,10 @@
 	//any other code needed for addItem page goes here
 	
 });
-  
+  function gu(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
   
   
   
@@ -55,6 +58,12 @@
  
  
  function displayData() {
+	 key = this.key
+	 // Clear the Display
+	 $("span").show(key);
+	 
+	 console.log("This is line 62 with the key " +key);
+	 
 	 $('recordmakeCreature');
 	 
 	 $('#recordmakeCreature').html();
@@ -75,8 +84,111 @@
 	 // You can do that by
 	 // $('#myBtn').click(function(e){ // code goes here }); 
 	 
-	 $("#button_id").click(function() { 
-alert(localStorage.getItem('local_key')); // #local_key i have at the time of storing data into local storage using localStorage.setItem
+	 
+		 $("#CreatureClear").click(function(){
+			 console.log("line 84");
+			 ///////////////////////
+		function getMyData() {
+			console.log("in the getmy data funtion line 89");
+		toggleMyControls("on");
+		if (localStorage.length === 0) {
+			alert("There is no data in Local Storeage. I have loaded default data.");
+			getMyDefaultData();
+		} 
+		//Write information for the Local Storeage to the brower.
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		// document.body is writeing the user contents to the screen
+		// for the user to see.
+		document.body.appendChild(makeDiv);
+		gu('items').style.display = "block";
+		for (var i = 0, len =localStorage.length; i<len;i++) {
+			console.log("localStorge.length ="+ i +"")
+			var makeli = document.createElement('li');
+			var createLinks = document.createElement('li');
+			makeList.appendChild(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			// make from string to an object using json
+			var obj = JSON.parse(value);
+		        var makeSubLink = document.createElement('ul');
+			makeli.appendChild(makeSubLink);
+			// Makeing a sublist image dynamicly for each catagory of the list
+			//getMyCustomImage(obj.groups[1],makeSubLink);
+		//	console.log("groups")+ groups[1] + makeSubLink + " makesublink here";
+			for (var n in obj) {
+				var makeSubli = document.createElement('li');
+		 		makeSubLink.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+				makeSubLink.appendChild(createLinks);
+		
+			}
+			// makeEditAndDeleteLinks(localStorage.key(i), createLinks); // create our edit and delete links
+		}
+	}
+	//Get the image for the right category that is being displayed
+	function getMyCustomImage(pictureName, makeSubLink) {
+		var createLinks = document.createElement('li');
+		makeSubLink.appendChild(createLinks);
+		var newImg = document.createElement('img');
+		var setSrc = newImg.setAttribute("src", "images/"+ pictureName + ".png");
+		createLinks.appendChild(newImg);
+		
+		
+		
+	}
+	
+		
+		
+		/////////////////////
+			  $(".inner").append("<p>Test</p>");
+			  $(".inner").append("<h1>Test</h1>");
+			  
+			  this.$OuterDiv = $('<div></div>')
+    
+    .append($('<table></table>')
+        .attr({ cellSpacing : 0 })
+        .addClass("text")
+    )
+;
+		
+			// $("div").hide();
+			 console.log("This is line 156 with the key " +key);
+			 
+			 getMyData();
+			 
+			 $(".inner").append("<p>Test</p>");
+			 
+			 console.log(" append here line 160");
+			 
+			
+			$('#CreatureClear').on('click', function(e) {
+    e.preventDefault(); 
+	        $('#CreatureClear').on('click', getMyData);
+	                                      
+    var name = $('#CreatureClear').val().toUpperCase();
+    alert(name);
+	
+});
+
+$('#delete').on('click',function(e){
+	e.preventDefault();
+	
+	
+	
+	var myKey = $(this).data('key');
+	
+	localStorage.removeItem(myKey);
+});
+			 
+			 
+	 
+//alert(localStorage.getItem('local_key')); // #local_key i have at the time of storing data into local storage using localStorage.setItem
+
+// display data to the screen.
 
 })
 	 
@@ -129,6 +241,31 @@ alert(localStorage.getItem('local_key')); // #local_key i have at the time of st
 var autofillData = function (){
 	 
 };
+
+function toggleMyControls(n) {
+	switch(n) {
+		case "on":
+		
+	
+		//gu('clear').style.display = "inline";
+		//gu('toggle').style.display = "inline";
+		//gu('displayData').style.display = "none";
+		
+		break;
+	        case "off":
+		gu('pizzaForm').style.display = "block";
+		gu('clear').style.display = "inline";
+		gu('toggle').style.display = "block";
+		gu('displayData').style.display = "block";
+		 
+		break;
+	     default:
+		return false;
+	}
+}
+
+
+
 
 var getData = function(){
 
